b44743d4dabe5f366f9fd49728b2b62cfd550c3c
Josephkimeu7961 <josephkimeu7961@gmail.com>
2024-10-02 12:45:30 -0700

    Update Loan application.c

--- Loan application.c
+++ Loan application.c
@@ -1,7 +1,12 @@
+//program to offer a loan
+/*Author:Joseph
+Reg No:CT102/G/22241/24
+Date:02/10/2024
+  */
 #include <stdio.h>
 
 int main() {
-  int age,annual_income;
+  int age,annual_income;//%d
   printf("Enter age:");
   scanf("%d",&age);
   printf("Enter annual_income:");
