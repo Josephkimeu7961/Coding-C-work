0d653dc5d4f7a746e522f5c3e48081fa9ef8f334
Josephkimeu7961 <josephkimeu7961@gmail.com>
2024-10-18 10:56:13 +0300

    Add files via upload

---
+++ book structure.c
@@ -0,0 +1,30 @@
+//Program to determine structure
+/*Author:Joseph
+Reg No:CT102/G/22241/24
+*/
+#include <stdio.h>
+#include <string.h>
+
+struct book{
+	char title[30];
+	char author[30];
+	int publicationYear;
+	char ISBN[13];
+	float price;
+}book;
+
+int main(){
+	strcpy(book.title,"Introduction to C Programming");
+	strcpy(book.author,"John Smith");
+	book.publicationYear=2022;
+	strcpy(book.ISBN,"9780131103627");
+	book.price=49.99;
+	
+	printf("title:%s\n",book.title);
+	printf("author:%s\n",book.author);
+	printf("publication year:%d\n",book.publicationYear);
+    printf("ISBN:%s\n",book.ISBN);
+ 	printf("price:%f\n",book.price);
+ 	
+ 	return 0;
+}
